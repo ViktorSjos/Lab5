f1bfdc347590a9104bebac9d001c626ff18b9485
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-12 15:51:21 +0100

    Update PickingEvent.java

--- src/lab5/events/PickingEvent.java
+++ src/lab5/events/PickingEvent.java
@@ -3,27 +3,34 @@ package lab5.events;
 import lab5.state.*;
 
 public class PickingEvent extends Event {
-	private StoreState state;
+
+	private StoreState sState;
 	private EventQueue eQueue;
 	private CustomerQueue cQueue;
 
 	/**
+	 * The constructor
+	 * 
 	 * @param Tar in 
 	 */
-	private pickingEvent(StoreState state) {
-		super()
-		this.state = state;
-		this.eQueue = eQueue;
-		this.cQueue = cQueue;
+	public PickingEvent(SimState state, EventQueue queue) {
+		super(state, queue);
 	}
 
+	/**
+	 * If there are free registers paying event gets sent to eventQueue, otherwise
+	 * send customer to CustomerQueue
+	 */
 	private void runEvent() {
-		if (state.getFreeRegister() > 0) {
-			eQueue.AddEvent(new PayingEvent());
-			state.changeFreeRegisters(-1);
-			state.changeCustomersPaying(+1);
+		if (sState.getFreeRegister() > 0) {
+			sState.changeCurrentCustomer(id);
+			Event paying = new PayingEvent(Timer.timeToPay());
+			sState.changeCustomersShopping(-1);
+			sState.changeFreeRegisters(-1);
+			sState.changeCustomersPaying(+1);
+			eQueue.AddEvent(paying);
 		} else {
-			cQueue.addToArray();
+			cQueue.addToArray(id);
 		}
 	}
 }
