4edac9ed82f3e1a165c4abe18e4d46b461804442
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-12 02:38:10 +0100

    Update CustomerQueue.java

--- src/lab5/state/CustomerQueue.java
+++ src/lab5/state/CustomerQueue.java
@@ -1,5 +1,32 @@
 package lab5.state;
 
+import java.util.ArrayList;
+
 public class CustomerQueue {
-    
+	private ArrayList<Integer> customerQueue = new ArrayList<Integer>();
+
+	/**
+	 * Adds the customer id to the array
+	 * 
+	 * @param customer id
+	 */
+	public void addToArray(int customer) {
+		customerQueue.add(customer);
+	}
+
+	/**
+	 * Returns the length of the queue
+	 * 
+	 * @return length of queue
+	 */
+	public int getCustomerQueueLength() {
+		return customerQueue.size();
+	}
+
+	/**
+	 * Removes the first customer in line
+	 */
+	public void removeFirstInLine() {
+		customerQueue.remove(customerQueue.get(0));
+	}
 }
