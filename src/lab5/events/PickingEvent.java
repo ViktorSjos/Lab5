23c0ac9a83087536ad7235ea62af8768bd61f9b7
ViktorSjos <98907698+ViktorSjos@users.noreply.github.com>
2022-03-13 19:49:20 +0100

    Update PickingEvent.java

--- src/lab5/events/PickingEvent.java
+++ src/lab5/events/PickingEvent.java
@@ -8,25 +8,27 @@ public class PickingEvent extends Event {
 	private EventQueue eQueue;
 	private CustomerQueue cQueue;
 	private CreateCustomer customer;
+	double ExTime;
 
 	/**
 	 * The constructor
 	 * 
 	 * @param Tar in
 	 */
-	public PickingEvent(StoreState sState, EventQueue eQueue, CustomerQueue cQueue, CreateCustomer customer, double time) {
+	public PickingEvent(StoreState sState, EventQueue eQueue, CustomerQueue cQueue, CreateCustomer customer) {
 		super(time, "Picking");
 		this.sState = sState;
 		this.eQueue = eQueue;
 		this.cQueue = cQueue;
 		this.customer = customer;
+		ExTime = this.ExecutionTime(state.getCurrentTime+Timer.timeToPick());
 	}
 
 	/**
 	 * If there are free registers paying event gets sent to eventQueue, otherwise
 	 * send customer to CustomerQueue
 	 */
-	private void ExecuteEvent() {
+	private void Execute() {
 		if (sState.getFreeRegister() > 0) {
 			sState.changeCurrentCustomer(customer.GetId);
 			Event2 paying = new PayingEvent(sState, eQueue, cQueue, sState.getCurrentTime() + Timer.timeToPay());
