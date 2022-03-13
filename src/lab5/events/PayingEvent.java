0369370f48479fa397994845b5a5c9d9be08a19c
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-13 20:22:53 +0100

    Update PayingEvent.java

--- src/lab5/events/PayingEvent.java
+++ src/lab5/events/PayingEvent.java
@@ -1,6 +1,8 @@
 
 package lab5.events;
 
+import java.util.Timer;
+
 import lab5.state.*;
 
 public class PayingEvent extends Event {
@@ -8,37 +10,34 @@ public class PayingEvent extends Event {
 	private StoreState sState;
 	private EventQueue eQueue;
 	private CustomerQueue cQueue;
-	private Customer customer;
+	private int Customer;
 	private double ExTime;
 
-	public PayingEvent(StoreState sState, EventQueue eQueue, CustomerQueue cQueue) {
-		super(time, "Paying");
+	public PayingEvent(StoreState sState, EventQueue eQueue, CustomerQueue cQueue, double time) {
+		super(sState, eQueue);
 		this.sState = sState;
 		this.eQueue = eQueue;
 		this.cQueue = cQueue;
-		ExTime = this.ExecutionTime(state.getCurrentTime+Timer.timeToPay());
+		ExTime = this.ExecutionTime(sState.getCurrentTime() + Timer.timeToPay());
 	}
 
 	/**
 	 * Runs the event
 	 */
-	public void Execute() {
-		sState.changeCurrentCustomer(id);
+	public void ExecuteEvent() {
+		sState.changeCurrentCustomer(Customer);
+		sState.changeName("Paying");
 		sState.changeCustomersPaying(-1);
+		sState.UpdateTimeLedigaKassor();
 		sState.changeFreeRegisters(1);
 
 		if (cQueue.getCustomerQueueLength() > 0) {
-			Event2 paying = new PayingEvent(sState, eQueue, cQueue.getFirstInLine(),
-					sState.getCurrentTime() + Timer.timeToPay());
+			Event paying = new PayingEvent(sState, eQueue, cQueue.getFirstInLine(), Customer, ExTime);
 			sState.changeCustomersShopping(-1);
 			sState.changeFreeRegisters(-1);
 			sState.changeCustomersPaying(+1);
 			eQueue.AddEvent(paying);
 			cQueue.removeFirstInLine();
-		}
-		// Tror att det här ska vara ett förinställt event istället
-		if (sState.getCurrentTime() >= 10 && sState.Open == true) {
-			Event2 closed = new ClosingEvent();
 		}
 	}
 }
