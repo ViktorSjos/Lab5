cd27fc92b4307e0670b82a6903caef26d1d383be
ViktorSjos <98907698+ViktorSjos@users.noreply.github.com>
2022-03-13 19:10:23 +0100

    Add files via upload

--- src/lab5/events/CustomerArrival.java
+++ src/lab5/events/CustomerArrival.java
@@ -1,30 +1,46 @@
 package lab5.events;
 
+import java.util.Timer;
+
 import lab5.state.CreateCustomer.Customer;
 import lab5.state.SimState;
 import lab5.state.StoreState;
 
 public class CustomerArrival extends Event{
 	
-	int customer;
-	StoreState storeState;
+	int Customer;
+	double ExTime;
+	StoreState state;
+	EventQueue queue;
 
-	public CustomerArrival(SimState state, EventQueue queue, int Customer) {
+	public CustomerArrival(StoreState state, EventQueue queue) {
 		super(state, queue);
-		this.customer = Customer;
-		this.ExecutionTime(100); //calculate time
+		this.state = state;
+		this.queue = queue;
+		ExTime = this.ExecutionTime(state.getCurrentTime+Timer.timeToNextCustomer()); //calculate time
 	}
 	
 	public void Execute() {
-		SimState state = super.getState();
+		StoreState.CurrentTime() = this.ExTime;
+		
+		//SEND CUTOMER ARRIVED TO VIEW
 		
 		if(!state.RunCheck()) {
 			return;
 		}
-		if(!storeState.SpaceAvalible()) { //SpaceAvalible ökar missed customers om den retunerar false.
+		if(!state.SpaceAvalible()) {
 			return;
 		}
-		storeState.CustomerArrived();  // den här ökar bara antalet kunder i affären nu.
+		//Create this customer
+		this.Customer = state.AddCustomer();
+		queue.AddEvent(PlockEvent(state, queue, this.Customer));
+		
+		//Add next cutomer
+		CustomerArrival FirstCustomer = new CustomerArrival(state, queue);
+		queue.AddEvent(FirstCustomer);
+		
+		
+		
 		
 	}
 	
