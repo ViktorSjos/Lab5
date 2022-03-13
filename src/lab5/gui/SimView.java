43e7659fb5051e7f0ede0c6c34d8c375c0bc6ea7
felickswocksblom <98887899+felickswocksblom@users.noreply.github.com>
2022-03-13 19:59:49 +0100

    Update SimView.java

--- src/lab5/gui/SimView.java
+++ src/lab5/gui/SimView.java
@@ -1,92 +1,41 @@
 package lab5.gui;
 
-
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class SimView extends JPanel{
+public class SimView {
+	
+	static int N;
+	static int M;
+	static int lambda;
+	static int P_min;
+	static int P_max;
+	static int K_min;
+	static int K_max;
+	static int f;
 	
 	
-
-
-	int N;
-	int M;
-	int lambda;
-	int P_min;
-	int P_max;
-	int K_min;
-	int K_max;
-	int f;
 	
-	public SimView() {
+	public static void SimView() {
 		
-		JFrame frame = new JFrame();
-		
-		JLabel label1 = new JLabel("Parametrar");
-		JLabel label2 = new JLabel("=============");
-		JLabel label3 = new JLabel("Antal kassor: " + N);
-		JLabel label4 = new JLabel("Max som ryms: " + M);
-		JLabel label5 = new JLabel("Ankomsthastighet: " + lambda);
-		JLabel label6 = new JLabel("Plocktider: [" + P_min + ", " + P_max +"]");
-		JLabel label7 = new JLabel("Betaltider: [" + K_min + ", " + K_max +"]");
-		JLabel label8 = new JLabel("Frö: " + f);
-		JLabel label9 = new JLabel("     ");
-		JLabel label10 = new JLabel("FÖRLOPP");
-		JLabel label11 = new JLabel("========");
-		JLabel label12 = new JLabel("Tid       Händelse       	Kund	       ?	       "
-				+       	"led	       ledT	       I	       $	       :-(	"
-				+ "       	köat	       köT	       	köar"
-				+ "		        [Kassakö]");
+		System.out.println("Parametrar");
+		System.out.println("==========");
+		System.out.println("");
+		System.out.println("Antal Kassor: " + N);
+		System.out.println("Max som ryms: " + M);
+		System.out.println("Ankomsthastighet: " + lambda);
+		System.out.println("Plocktider: [" + P_min + ", " + P_max +"]");
+		System.out.println("Betaltider: [" + K_min + ", " + K_max +"]");
+		System.out.println("Frö: " + f);
+		System.out.println("");
+		System.out.println("Förlopp");
+		System.out.println("=======");
+		System.out.println("Tid   Händelse   Kund   ?   led   ledT   I   $   :-(   köat   köT   köar   Kassakö");
+		for(int i = 0; i < EventQueue)	
 		
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 500, 200));
-		panel.setLayout(new GridLayout(0,1));
 		
-		panel.add(label1);
-		panel.add(label2);
-		panel.add(label3);
-		panel.add(label4);
-		panel.add(label5);
-		panel.add(label6);
-		panel.add(label7);
-		panel.add(label8);
-		panel.add(label9);
-		panel.add(label10);
-		panel.add(label11);
-		panel.add(label12);
-		
-		
-		//Här ska en forloop som skriver ut eventens värden va
-		
-		label10.setLocation(200, 200);
-		
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setTitle("Förlopp");
-		frame.pack();
-		
-		frame.setResizable(true);
-		frame.setVisible(true);
 	}
 	
-	
-
-	
-	
 	public static void main(String[] args) {
-		new SimView();
+		SimView();
 	}
 
 }
