fd4eca9a202215b976a4584478d1e96387acf51c
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-13 19:42:27 +0100

    Update PayingEvent.java

--- src/lab5/events/PayingEvent.java
+++ src/lab5/events/PayingEvent.java
@@ -1,45 +1,4 @@
-package lab5.events;
 
-import lab5.state.*;
-
-public class PayingEvent extends Event {
-
-	private StoreState sState;
-	private EventQueue eQueue;
-	private CustomerQueue cQueue;
-	int Customer
-
-	public PayingEvent(SimState state, EventQueue queue,int customer) {
-		super(state, queue);
-		this.customer=Customer
-	}
-
-	/**
-	 * Runs the event
-	 * 
-	 * 
-	 */
-	public void runEvent() {
-		sState.changeCurrentCustomer(id); // id ligger nu lagrat i event. 
-		sState.changeCustomersPaying(-1);
-		sState.changeFreeRegisters(1);
-
-		if (cQueue.getCustomerQueueLength() > 0) {
-			// Vet inte hur jag ska ta in nuvarande tiden + tiden det tar för eventen
-			Event paying = new PayingEvent(CurrentTime + Timer.timeToPay(), cQueue.getFirstInLine());
-			sState.changeCustomersShopping(-1);
-			sState.changeFreeRegisters(-1);
-			sState.changeCustomersPaying(+1);
-			eQueue.AddEvent(paying);
-			cQueue.removeFirstInLine();
-		}
-		if (CurrentTime >= 10 && sState.Open == true) { // closingevent borde väl skapas i början av allt?
-			// Behöver ha några andra parametrar
-			Event closed = new ClosingEvent(state, eQueue);
-		}
-	}
-}
-/*
 package lab5.events;
 
 import lab5.state.*;
@@ -61,7 +20,7 @@ public class PayingEvent extends Event2 {
 	/**
 	 * Runs the event
 	 */
-	public void runEvent() {
+	public void ExecuteEvent() {
 		sState.changeCurrentCustomer(id);
 		sState.changeCustomersPaying(-1);
 		sState.changeFreeRegisters(1);
@@ -81,4 +40,3 @@ public class PayingEvent extends Event2 {
 		}
 	}
 }
-*/
