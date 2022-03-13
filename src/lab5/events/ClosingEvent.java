2f76d7378abd849563d52ae2f931d4d7fabfbbef
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-13 19:34:22 +0100

    Update ClosingEvent.java

--- src/lab5/events/ClosingEvent.java
+++ src/lab5/events/ClosingEvent.java
@@ -2,11 +2,12 @@ package lab5.events;
 
 import lab5.state.*;
 
-public class ClosingEvent extends Event {
+public class ClosingEvent extends Event2 {
 	private StoreState sState;
 
-	public ClosingEvent(SimState state, EventQueue queue) {
-		super(state, queue);
+	public ClosingEvent(StoreState sState, EventQueue queue, CustomerQueue cQueue, double time) {
+		super(time, "Closing");
+		this.sState = sState;
 	}
 
 	/**
