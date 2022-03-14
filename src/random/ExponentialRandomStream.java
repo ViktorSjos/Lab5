77186e91a0ce2a62debc4a8a2c6c4955525306b5
Jonathanwes <75334370+Jonathanwes@users.noreply.github.com>
2022-03-14 18:30:48 +0100

    Update ExponentialRandomStream.java

--- src/random/ExponentialRandomStream.java
+++ src/random/ExponentialRandomStream.java
@@ -1,25 +1,45 @@
-// creates random time for arrivaltime
-
-*@author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
 
 package random;
 
 import java.util.Random;
+
+/**
+ * generates random numbers
+ * 
+ * @author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
+ */
 
 public class ExponentialRandomStream {
 
 	private Random rand;
 	private double lambda;
 
+	/**
+	 * Constructor takes two arguments
+	 * 
+	 * 
+	 * @param lambda
+	 * @param seed
+	 */
 	public ExponentialRandomStream(double lambda, long seed) {
 		rand = new Random(seed);
 		this.lambda = lambda;
 	}
 
+	/**
+	 * Constructor takes one argument
+	 * 
+	 * @param lambda
+	 */
 	public ExponentialRandomStream(double lambda) {
 		rand = new Random();
 		this.lambda = lambda;
 	}
+
+	/**
+	 * 
+	 * @return returns random number
+	 */
 
 	public double next() {
 		return -Math.log(rand.nextDouble()) / lambda;
