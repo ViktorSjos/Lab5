eef4d60be40bb3f7b3a8a06218ab89bda80fd1e8
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-14 17:33:51 +0100

    Update PayingEvent.java

--- src/events/PayingEvent.java
+++ src/events/PayingEvent.java
@@ -4,6 +4,12 @@ import GeneralSim.Event;
 import GeneralSim.EventQueue;
 import state.*;
 
+/**
+ * A class representing an event where the customer pays and leaves
+ * 
+ * @author Jonathan Westerdahl, Felix Woxblom, Isak Sandegren, Viktor Sjöstedt
+ *
+ */
 public class PayingEvent extends Event {
 
 	private StoreState sState;
@@ -12,6 +18,14 @@ public class PayingEvent extends Event {
 	private int Customer;
 	private double ExTime;
 
+	/**
+	 * The constructor
+	 * 
+	 * @param sState   Object of the StoreState class
+	 * @param eQueue   Object of the EventQueue class
+	 * @param customer The customer ID
+	 * @param cQueue   Object of the CustomerQueue class
+	 */
 	public PayingEvent(StoreState sState, EventQueue eQueue, int customer, CustomerQueue cQueue) {
 		super(sState, eQueue);
 		this.sState = sState;
@@ -40,14 +54,12 @@ public class PayingEvent extends Event {
 			sState.UpdateObs();
 			eQueue.AddEvent(paying);
 			cQueue.removeFirstInLine();
-			
-		} 
-			
-			
-		
-		
+		}
 	}
 
+	/**
+	 * Gets the new time
+	 */
 	public double GetExecutionTime() {
 		return this.ExTime;
 	}
