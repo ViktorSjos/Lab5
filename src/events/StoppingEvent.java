37b0199261bf3bc0be4d9a7c841287bae9ddef7b
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-14 17:35:49 +0100

    Update StoppingEvent.java

--- src/events/StoppingEvent.java
+++ src/events/StoppingEvent.java
@@ -5,12 +5,23 @@ import GeneralSim.Event;
 import GeneralSim.EventQueue;
 import state.StoreState;
 
+/**
+ * A class representing an event stopping the simulation
+ * 
+ * @author Jonathan Westerdahl, Felix Woxblom, Isak Sandegren, Viktor Sjöstedt
+ *
+ */
 public class StoppingEvent extends Event {
-
 	StoreState state;
 	EventQueue queue;
 	double ExTime;
 
+	/**
+	 * The constructor
+	 * 
+	 * @param state Object of the class StoreState
+	 * @param queue Object of the class EventQueue
+	 */
 	public StoppingEvent(StoreState state, EventQueue queue) {
 		super(state, queue);
 		this.queue = queue;
@@ -18,8 +29,10 @@ public class StoppingEvent extends Event {
 		this.state = state;
 	}
 
+	/**
+	 * Runs the event
+	 */
 	public void Execute() {
-
 		queue.RemoveEvent();
 		state.ChangeName("Stop");
 		state.SimStopTime(this.ExTime);
@@ -28,6 +41,9 @@ public class StoppingEvent extends Event {
 
 	}
 
+	/**
+	 * Gets the time
+	 */
 	public double GetExecutionTime() {
 		return this.ExTime;
 	}
