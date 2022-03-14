7fe310af2df5630a108adc73683f5664f5f49b9e
Jonathanwes <75334370+Jonathanwes@users.noreply.github.com>
2022-03-14 18:32:47 +0100

    Update UniformRandomStream.java

--- src/random/UniformRandomStream.java
+++ src/random/UniformRandomStream.java
@@ -1,21 +1,36 @@
-//creates randomised time for pickingtime and payingtime
-
-*@author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
-
 package random;
 
 import java.util.Random;
 
+/**
+ * returns random values
+ * 
+ * @author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
+ */
 public class UniformRandomStream {
 
 	private Random rand;
 	private double lower, width;
 
+	/**
+	 * Constructor takes three arguments
+	 * 
+	 * @param lower
+	 * @param upper
+	 * @param seed
+	 */
 	public UniformRandomStream(double lower, double upper, long seed) {
 		rand = new Random(seed);
 		this.lower = lower;
 		this.width = upper - lower;
 	}
+
+	/**
+	 * constructor takes two arguments
+	 * 
+	 * @param lower
+	 * @param upper
+	 */
 
 	public UniformRandomStream(double lower, double upper) {
 		rand = new Random();
@@ -23,6 +38,10 @@ public class UniformRandomStream {
 		this.width = upper - lower;
 	}
 
+	/**
+	 * 
+	 * @return returns the next random value
+	 */
 	public double next() {
 		return lower + rand.nextDouble() * width;
 	}
