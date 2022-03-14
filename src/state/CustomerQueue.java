5c87a966e2d608039c6a14490d8afd76aecd72df
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-14 17:35:14 +0100

    Update CustomerQueue.java

--- src/state/CustomerQueue.java
+++ src/state/CustomerQueue.java
@@ -1,19 +1,23 @@
-// Class resposible for creating the customerQueue
-
-
-*@author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
-
+/**
+ * 
+ */
 package state;
 
 import java.util.ArrayList;
 
+/**
+ * A class representing a customer queue;
+ * 
+ * @author Jonathan Westerdahl, Felix Woxblom, Isak Sandegren, Viktor Sjöstedt
+ *
+ */
 public class CustomerQueue {
 	private ArrayList<Integer> customerQueue = new ArrayList<Integer>();
 
 	/**
 	 * Adds the customer id to the array
 	 * 
-	 * @param customer id
+	 * @param The customer ID
 	 */
 	public void addToArray(int customer) {
 		customerQueue.add(customer);
@@ -22,12 +26,17 @@ public class CustomerQueue {
 	/**
 	 * Returns the length of the queue
 	 * 
-	 * @return length of queue
+	 * @return Length of queue
 	 */
 	public int getCustomerQueueLength() {
 		return customerQueue.size();
 	}
-	
+
+	/**
+	 * Gets the array
+	 * 
+	 * @return The array
+	 */
 	public ArrayList<Integer> GetCustomerQueue() {
 		return customerQueue;
 	}
@@ -42,7 +51,7 @@ public class CustomerQueue {
 	/**
 	 * Returns the first customer in line
 	 * 
-	 * @return position 0 in array
+	 * @return Position 0 in array
 	 */
 	public int getFirstInLine() {
 		return customerQueue.get(0);
