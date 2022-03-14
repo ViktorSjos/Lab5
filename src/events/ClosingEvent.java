1874744bdfe765773c8309f4d9cbf3e353109d00
isaksandegren <100279410+isaksandegren@users.noreply.github.com>
2022-03-14 17:33:16 +0100

    Update ClosingEvent.java

--- src/events/ClosingEvent.java
+++ src/events/ClosingEvent.java
@@ -5,17 +5,29 @@ import GeneralSim.Event;
 import GeneralSim.EventQueue;
 import state.*;
 
+/**
+ * A class representing an event closing the store
+ * 
+ * @author Jonathan Westerdahl, Felix Woxblom, Isak Sandegren, Viktor Sjöstedt
+ *
+ */
 public class ClosingEvent extends Event {
 	private StoreState sState;
 	private EventQueue queue;
 	private double ExTime;
 
+	/**
+	 * The constructor
+	 * 
+	 * @param sState Object for the StoreState class
+	 * @param queue  Object for the EventQueue class
+	 * @param time   The current time
+	 */
 	public ClosingEvent(StoreState sState, EventQueue queue, double time) {
 		super(sState, queue);
 		this.sState = sState;
 		this.queue = queue;
 		ExTime = time;
-
 	}
 
 	/**
@@ -31,6 +43,9 @@ public class ClosingEvent extends Event {
 		sState.UpdateObs();
 	}
 
+	/**
+	 * Gets the new time
+	 */
 	public double GetExecutionTime() {
 		return this.ExTime;
 	}
