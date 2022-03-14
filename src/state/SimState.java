df7b238239a853c682e94ffb96f421d8c7aada8d
Jonathanwes <75334370+Jonathanwes@users.noreply.github.com>
2022-03-14 17:35:13 +0100

    Update SimState.java

--- src/state/SimState.java
+++ src/state/SimState.java
@@ -1,34 +1,28 @@
-//changes the state of the simulation
-
-*@author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
-
 package state;
 
 import java.util.Observable;
-
+/**
+ * @author Jonathan Westerdahl, Felix Woxblom,Isak Sandegren,Viktor Sjöstedt
+ *      Represent general simulation state
+ * 
+ */
 public class SimState extends Observable {
 
-	boolean Running = false;  //variable that is true if the simulation is running
-	double CurrentTime = 0;   
+	boolean Running = false;
+	double CurrentTime = 0;
 
 	public SimState() {
 
 	}
-	
-	//starts simulation by setting running to true
 
 	public void StartSim() {
 		Running = true;
 	}
-	
-	//stops simulation by setting running to false
 
 	public void StopSim() {
 		Running = false;
 		return;
 	}
-	
-	//returns running
 
 	public boolean RunCheck() {
 		return Running;
